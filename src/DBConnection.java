19b56162749216b135c5bfbda294227ae4e97c21
Кирилл <ustinovk722@gmail.com>
2026-06-11 18:03:47 +0300

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     public static final String URL = "jdbc:mysql://localhost:3306/tasktrecker_db?useSSL=false&serverTimezone=UTC";
     public static final String user = "root";
-    public static final String password = "wtuop75g0aL-6gS";
+    public static final String password = "YOUR_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, user, password);
