11314137486fe49c7837593f76b873c07a9b1fcc
KirillUstin <ustinovk722@gmail.com>
2026-06-24 23:47:09 +0300

    Небольшие изменения в файле подключения к базе данных

--- src/main/java/com/kirk/DBConnection.java
+++ src/main/java/com/kirk/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/tasktrecker_db";
-    private static final String user = "task_user";
-    private static final String password = "passw1010_qqp";
+    private static final String user = "YOUR_USER";
+    private static final String password = "YOUR_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, user, password);
